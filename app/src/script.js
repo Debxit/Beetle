ca6d2c0342eedd7db07cdc212a82a36a822283bd
olga <debxit@gmail.com>
2017-04-06 17:55:16 +0300

    blog, history, footer, mobile version

--- app/src/script.js
+++ app/src/script.js
@@ -21,5 +21,35 @@ $(document).ready(function () {
         cssEase: 'linear'
     });
 
+    $('.blog-slider').on('init', function (event, slick) {
+        var all = slick.slideCount;
+        if (all < 10) {
+            all = '0' + all
+        }
+
+        $('.blog-slider-nav__index .all').html(all);
+    });
+
+    $('.blog-slider').slick({
+        arrows: false
+    });
+
+    $('.blog-slider-nav__prev').click(function () {
+        $('.blog-slider').slick('slickPrev');
+    });
+
+    $('.blog-slider-nav__next').click(function () {
+        $('.blog-slider').slick('slickNext');
+    });
+
+
+    $('.blog-slider').on('afterChange', function (event, slick, currentSlide) {
+        var current = currentSlide +1;
+        if (current < 10) {
+            current = '0' + current;
+        }
+        $('.blog-slider-nav__index .current').html(current);
+    });
+
 
 });
